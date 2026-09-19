d392600838fe9ccd41267fdb51d70abee7426509
Lokmane <223161425+lokkdevv@users.noreply.github.com>
2026-09-19 16:35:25 -0400

    fixed little error with the git commit which broke the project

--- src/input/linux_input.h
+++ src/input/linux_input.h
@@ -1,10 +1,7 @@
 #pragma once
 
-<<<<<<< HEAD
-=======
 #define _DEFAULT_SOURCE
 
->>>>>>> 103097f (Fixed the problem where compiling on linux didn't work because a macro was missing)
 #include <termios.h>
 #include <unistd.h>
 #include <fcntl.h>
